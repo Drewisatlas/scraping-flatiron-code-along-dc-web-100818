41efe0a2b324b79eb8feb0ae2871bd4e43b64d38
Drewisatlas <drewisatlas@gmail.com>
2019-04-02 18:37:54 -0400

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,21 @@
+#start by examining the site we will be scraping
+#it looks like each course has a title, a schedule, and a description.
+#us this to define the class
 
+class Course
+  attr_accessor :title, :schedule, :description
 
+  @@all = [] #class variable that will store all Course instances
+
+  def initialize #everytie an instance of the class is initialized, we should push it into the @@all class variable
+    @@all << self
+  end
+
+  def self.all #returns to @@all array
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,13 +1,47 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-require_relative './course.rb'
+require_relative './course.rb' # we are requiring our Course class file so our scraper can use it to make new courses
+
+# responsible for using nokogiri to scrape the relevant data
+# Responsible for taking that data and using it to instantiate instances of the Course class.
 
 class Scraper
-  
-    def print_courses
-    self.make_courses
-    Course.all.each do |course|
+
+  def get_page
+    # get the HTML from the webpage
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    #NOTE! we broke up the rest of the function into the other methods
+    #iterate over the collection of nokogiri elements we want to pull information from. the post class stores course information
+    # doc.css('.post').each do |post|
+    #   course = Course.new #instantiate a new course on each iteration
+    #   course.title = post.css('h2').text #doc.css('.post').css("h2").text
+    #   course.schedule = post.css('.date').text #doc.css('.post').css(".date").text
+    #   course.description = post.css('p').text #doc.css('.post').css("p").text
+    # end
+  end
+
+  def get_courses #grabs all the HTML elements that contain a Course
+    #returns a collection of nokogiri XML elements, each describing a course
+    self.get_page.css('.post')
+  end
+
+  def make_courses #instantiates Course objects
+    #iterate over the collection of nokogiri elements we want to pull information from. the post class stores course information
+    #gives each course the correct title, schedule and description attribute
+    self.get_courses.each do |post|
+      course = Course.new #instantiate a new course on each iteration
+      course.title = post.css('h2').text #doc.css('.post').css("h2").text
+      course.schedule = post.css('.date').text #doc.css('.post').css(".date").text
+      course.description = post.css('p').text #doc.css('.post').css("p").text
+    end
+  end
+
+
+  def print_courses
+    self.make_courses # calls make_courses
+    Course.all.each do |course| #iterates over all of the courses, and puts out a list of the course offerings
       if course.title
         puts "Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
@@ -15,8 +49,7 @@ class Scraper
       end
     end
   end
-  
+
 end
 
-
-
+Scraper.new.print_courses # need to call the get page method to hit the pry, run with ruby lib/scraper.rb
